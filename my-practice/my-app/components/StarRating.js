82b7c106695b4bbdd8a4ad71368f743033b2bfe9
George K <gkurkjian@myseneca.ca>
2025-05-28 17:20:14 -0400

    second commit: included comments in StarRating.js to clear up how does it functions/works

--- my-practice/my-app/components/StarRating.js
+++ my-practice/my-app/components/StarRating.js
@@ -1,14 +1,18 @@
+// To use react-bootstrap-icons, install it from: https://www.npmjs.com/package/react-bootstrap-icons
 import { StarFill } from 'react-bootstrap-icons';
 import { Star } from 'react-bootstrap-icons';
 
-export default function StarRating({ rating }) {
+export default function StarRating({ rating }) {  // receives the rating value as a prop
 
-  let ratingInt = +rating
+  let ratingInt = +rating;  // convert rating to a number in case it's passed as a string
 
   return (
     <>
-      {[...Array(ratingInt)].map((e, i) => <StarFill key={i} />)}
-      {[...Array(5 - ratingInt)].map((e, i) => <Star key={i} />)}
+      {/* Render filled stars based on the rating value */}
+      {[...Array(ratingInt)].map((_, i) => <StarFill key={i} />)}
+
+      {/* Render empty stars to complete a total of 5 stars, which is why it's must 5 - ratingInt number */}
+      {[...Array(5 - ratingInt)].map((_, i) => <Star key={i} />)}
     </>
   );
 }
