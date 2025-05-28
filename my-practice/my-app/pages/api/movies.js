961e772788495321805334de85d8c8a82bffd62c
George K <gkurkjian@myseneca.ca>
2025-05-28 17:24:46 -0400

    third commit: comment included in movies.js to understand the Destruct HTTP method and displays it only on GET request and rejects other methods

--- my-practice/my-app/pages/api/movies.js
+++ my-practice/my-app/pages/api/movies.js
@@ -1,15 +1,19 @@
+// Import static movie data from a local file lib/movieData.js
 import getMovieData from "@/lib/movieData";
 
 export default function handler(req, res) {
-  const { method } = req; switch (method) {
+  // Destructure the HTTP method from the request object
+  const { method } = req;
+
+  switch (method) {
     case 'GET':
+      // If it's a GET request, return movie data as JSON
       res.status(200).json(getMovieData());
       break;
 
     default:
-      // send an error message back, indicating that the method is not supported by this route
-      res.setHeader('Allow', ['GET']);
+      // If any other method is used (POST, PUT, DELETE), reject it
+      res.setHeader('Allow', ['GET']);  // Tell the client only GET is allowed
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
-
